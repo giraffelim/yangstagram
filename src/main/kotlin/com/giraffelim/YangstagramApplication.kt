b17a8f7cec326fb050c270a8badcdc6753301c07
giraffelim <taeyang7704@naver.com>
2021-12-01 14:31:31 +0900

    initial commit

---
+++ src/main/kotlin/com/giraffelim/YangstagramApplication.kt
@@ -0,0 +1,11 @@
+package com.giraffelim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YangstagramApplication
+
+fun main(args: Array<String>) {
+    runApplication<YangstagramApplication>(*args)
+}
---
+++ src/test/kotlin/com/giraffelim/YangstagramApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.giraffelim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YangstagramApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
