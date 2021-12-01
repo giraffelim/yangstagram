7363f118effe59217b23ef29c6087c562cfb14ba
giraffelim <taeyang7704@naver.com>
2021-12-01 20:19:44 +0900

    feat: spring-security config 클래스 추가

---
+++ src/main/kotlin/com/giraffelim/config/SecurityConfig.kt
@@ -0,0 +1,40 @@
+package com.giraffelim.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http {
+            csrf { disable() }
+            authorizeRequests {
+                authorize("/auth/**", permitAll)
+                authorize("/**", authenticated)
+            }
+            formLogin {
+                loginPage = "/auth/login"
+                defaultSuccessUrl("/", true)
+            }
+            logout {
+                logoutUrl = "/auth/logout"
+                logoutSuccessUrl = "/auth/login"
+            }
+        }
+    }
+
+    override fun configure(web: WebSecurity?) {
+        web!!.ignoring().antMatchers("/css/**", "/images/**")
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+}
