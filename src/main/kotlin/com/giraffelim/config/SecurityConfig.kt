d3a49d99b4fadd1148ea53fe09b6c4d010488c1f
giraffelim <taeyang7704@naver.com>
2021-12-03 18:05:43 +0900

    refactor: http security authorize path 수정

--- src/main/kotlin/com/giraffelim/config/SecurityConfig.kt
+++ src/main/kotlin/com/giraffelim/config/SecurityConfig.kt
@@ -17,7 +17,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             csrf { disable() }
             authorizeRequests {
                 authorize("/auth/**", permitAll)
-                authorize("/users/**", permitAll)
+                authorize("/users/new", permitAll)
                 authorize("/**", authenticated)
             }
             formLogin {
