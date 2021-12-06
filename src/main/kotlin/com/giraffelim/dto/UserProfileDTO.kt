24ee5e7f07bd51aa0a34916d940caa5d7b073d53
giraffelim <taeyang7704@naver.com>
2021-12-06 19:13:39 +0900

    refactor: add property in UserEntity

--- src/main/kotlin/com/giraffelim/dto/UserProfileDTO.kt
+++ src/main/kotlin/com/giraffelim/dto/UserProfileDTO.kt
@@ -3,13 +3,27 @@ package com.giraffelim.dto
 import com.giraffelim.entity.User
 
 data class UserProfileDTO(
+    var id: Long? = null,
+    var name: String? = null,
+    var website: String? = null,
+    var bio: String? = null,
+    var email: String? = null,
     var username: String? = null,
-    var profileImgUrl: String? = null
+    var profileImgUrl: String? = null,
+    var gender: String? = null,
+    var phone: String? = null
 ) {
     companion object {
         fun create(user: User) = UserProfileDTO(
+            id = user.id,
+            name = user.name,
             username = user.username,
-            profileImgUrl = user.profileImageUrl
+            profileImgUrl = user.profileImageUrl,
+            bio = user.bio,
+            email = user.email,
+            gender = user.gender.toString(),
+            website = user.website,
+            phone = user.phone
         )
     }
 }
--- src/main/kotlin/com/giraffelim/entity/User.kt
+++ src/main/kotlin/com/giraffelim/entity/User.kt
@@ -4,6 +4,7 @@ import com.giraffelim.annotation.AllOpen
 import com.giraffelim.constant.Gender
 import com.giraffelim.constant.Role
 import com.giraffelim.dto.UserFormDTO
+import com.giraffelim.dto.UserProfileDTO
 import com.giraffelim.entity.audit.BaseEntity
 import org.springframework.security.crypto.password.PasswordEncoder
 import javax.persistence.*
@@ -37,7 +38,9 @@ class User(
     var profileImageUrl: String? = null,
 
     @Enumerated(EnumType.STRING)
-    var role: Role? = null
+    var role: Role? = null,
+
+    var website: String? = null
 ): BaseEntity() {
     companion object {
         fun createUser(userFormDTO: UserFormDTO, passwordEncoder: PasswordEncoder) = User(
@@ -48,4 +51,14 @@ class User(
             role = Role.USER
         )
     }
+
+    fun edit(userProfileDTO: UserProfileDTO) {
+        this.website = userProfileDTO.website
+        this.bio = userProfileDTO.bio
+        this.phone = userProfileDTO.phone
+        this.gender = userProfileDTO.gender?.let {
+            Gender.valueOf(it)
+        }
+    }
+
 }
